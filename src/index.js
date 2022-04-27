10bf8331d1b47afeaa375d75e48c214dc0290d8d
Kevin Poppe <kevin.poppe93@gmail.com>
2022-04-27 14:27:43 +0200

    modified cors

--- src/index.js
+++ src/index.js
@@ -15,7 +15,12 @@ const usersRouter = require('../routes/users');
 const gameRoute = require('../routes/game');
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
