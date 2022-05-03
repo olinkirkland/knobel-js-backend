6c40364079adbebcc728be14fa2084484a48b803
Kevin Poppe <Kevin@KevinPoppe.com>
2022-05-03 15:25:04 +0200

    added new route get currentgames

--- routes/cheat.js
+++ routes/cheat.js
@@ -4,38 +4,35 @@ const router = express.Router();
 const JWT = require('../controllers/JWT');
 const UserSchema = require('../models/UserSchema');
 
-router.post(
-  '/',
-  /* JWT.check, */ (req, res) => {
-    let xp = req.query.xp;
-    let gold = req.query.gold;
-    let lvl = req.query.lvl;
-    let id = req.body.userID;
-    let error = '';
+router.post('/', JWT.check, (req, res) => {
+  let xp = req.query.xp;
+  let gold = req.query.gold;
+  let lvl = req.query.lvl;
+  let id = req.body.userID;
+  let error = '';
 
-    console.log(`Player with ID ${id} is cheating`);
+  console.log(`Player with ID ${id} is cheating`);
 
-    if (typeof xp !== 'undefined') {
-      xp = xp < 0 ? xp * -1 : xp;
-      options = { experience: xp };
-    } else if (typeof gold !== 'undefined') {
-      gold = gold < 0 ? gold * -1 : gold;
-      options = { gold: gold };
-    } else if (typeof lvl !== 'undefined') {
-      lvl = lvl < 0 ? lvl * -1 : lvl;
-      options = { level: lvl };
-    }
-    UserSchema.findByIdAndUpdate({ _id: id }, options).catch((err) => {
-      error = err;
-      console.log('Error at Cheat', error);
-    });
-
-    if (error !== '') {
-      res.status(418).send('I am a Teapot!');
-    } else {
-      res.status(218).send('DoNe!11!!! ChEaTeR!!111!!');
-    }
+  if (typeof xp !== 'undefined') {
+    xp = xp < 0 ? xp * -1 : xp;
+    options = { experience: xp };
+  } else if (typeof gold !== 'undefined') {
+    gold = gold < 0 ? gold * -1 : gold;
+    options = { gold: gold };
+  } else if (typeof lvl !== 'undefined') {
+    lvl = lvl < 0 ? lvl * -1 : lvl;
+    options = { level: lvl };
   }
-);
+  UserSchema.findByIdAndUpdate({ _id: id }, options).catch((err) => {
+    error = err;
+    console.log('Error at Cheat', error);
+  });
+
+  if (error !== '') {
+    res.status(418).send('I am a Teapot!');
+  } else {
+    res.status(218).send('DoNe!11!!! ChEaTeR!!111!!');
+  }
+});
 
 module.exports = router;
--- routes/game.js
+++ routes/game.js
@@ -8,43 +8,38 @@ const Game = require('../classes/Game');
 
 const currentGames = [];
 
-router.post(
-  '/',
-  /* JWT.check, */ (req, res) => {
-    // Return an Array with Objects of Questions, depending on the choice of Options.
-    // Options:
-    // difficulty: easy || medium || hard
-    // type: mutliple || boolean (If empty: mutliple)
-    // amount: 1 - 50 (If empty: 10)
-    // category: One of all Categories as String (Request /categories to see Options)
+router.post('/', JWT.check, (req, res) => {
+  // Return an Array with Objects of Questions, depending on the choice of Options.
+  // Options:
+  // difficulty: easy || medium || hard
+  // type: mutliple || boolean (If empty: mutliple)
+  // amount: 1 - 50 (If empty: 10)
+  // category: One of all Categories as String (Request /categories to see Options)
 
-    const options = req.body;
-    gameHandler.getQuestions(options).then((response) => res.send(response));
+  const options = req.body;
+  gameHandler.getQuestions(options).then((response) => res.send(response));
+});
+
+router.get('/categories', JWT.check, (req, res) => {
+  // Return avaible Categories for Questions as an Array
+  res.send(gameHandler.categoriesCatalog());
+});
+
+router.post('/host', JWT.check, (req, res) => {
+  const options = req.body.options;
+
+  if (currentGames.find((el) => el.name === options.name)?.name) {
+    res.status(400).send('Name is unavaible');
+  } else {
+    const newGame = new Game(options);
+    currentGames.push(newGame);
+
+    res.status(201).send(newGame);
   }
-);
+});
 
-router.get(
-  '/categories',
-  /* JWT.check, */ (req, res) => {
-    // Return avaible Categories for Questions as an Array
-    res.send(gameHandler.categoriesCatalog());
-  }
-);
-
-router.post(
-  '/host',
-  /* JWT.check, */ (req, res) => {
-    const options = req.body.options;
-
-    if (currentGames.find((el) => el.name === options.name)?.name) {
-      res.status(400).send('Name is unavaible');
-    } else {
-      const newGame = new Game(options);
-      currentGames.push(newGame);
-
-      res.status(201).send(currentGames);
-    }
-  }
-);
+router.get('/currentgames', JWT.check, (req, res) => {
+  res.send(currentGames);
+});
 
 module.exports = router;
