c0a2a0f053303de651f4f98b9e32251433c85d6d
Olin Kirkland <me@olinkirk.land>
2022-05-05 20:32:29 +0200

    Change level up algo

--- utils/Util.js
+++ utils/Util.js
@@ -1,6 +1,6 @@
 function experienceNeededFromLevel(level) {
-  // 100 + (x/7)^2
-  return Math.round(100 + Math.pow(level / 7, 2));
+  // 100 + (x x 50)^(1/4)
+  return Math.round(100 + Math.pow(level * 50, 1 / 4));
 }
 
 module.exports = {
