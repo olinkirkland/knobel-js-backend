52a686cf87d09eb49dcbff74dfe9150c1c191b94
Olin Kirkland <me@olinkirk.land>
2022-05-06 17:05:26 +0200

    Added GameEventType.JOINED, implemented response for joining

--- classes/Game.js
+++ classes/Game.js
@@ -65,7 +65,9 @@ class Game {
 
     socket.join(this.roomID);
 
-    socket.emit("game-join-success", data);
+    Connection.instance.io
+      .to(this.roomID)
+      .emit(GameEventType.JOINED, { userID: user.id });
   }
 
   async onGameStart(socketID) {
