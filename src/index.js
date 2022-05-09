8b3d9f0c7aa902e0442f42bc3b888409f970c1f4
Kevin Poppe <Kevin@KevinPoppe.com>
2022-05-09 15:01:38 +0200

    added shop-system

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 // Dependencies
-const { Connection } = require('../controllers/Connection');
+const { Connection } = require("../controllers/Connection");
 
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 const app = express();
-const AvatarSchema = require('../models/AvatarSchema');
-const User = require('../models/UserSchema');
+const AvatarSchema = require("../models/AvatarSchema");
+const User = require("../models/UserSchema");
 
 // Routes
-const usersRouter = require('../routes/users');
-const gameRoute = require('../routes/game').router;
-const friendsRoute = require('../routes/friends');
-const cheatRoute = require('../routes/cheat');
+const usersRouter = require("../routes/users");
+const gameRoute = require("../routes/game").router;
+const friendsRoute = require("../routes/friends");
+const cheatRoute = require("../routes/cheat");
+const shopRoute = require("../routes/shopRoute");
 
 // Connection
 const connection = Connection.instance;
@@ -22,30 +23,31 @@ connection.connect(app);
 // Middlewares
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://localhost:8000'],
+    origin: ["http://localhost:3000", "http://localhost:8000"],
     credentials: true
   })
 );
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-app.use('/users', usersRouter);
-app.use('/game', gameRoute);
-app.use('/friends', friendsRoute);
-app.use('/cheat', cheatRoute);
+app.use("/users", usersRouter);
+app.use("/game", gameRoute);
+app.use("/friends", friendsRoute);
+app.use("/cheat", cheatRoute);
+app.use("/shop", shopRoute);
 
-app.get('/testcors', (req, res) => {
-  res.send('Hello world!!!');
+app.get("/testcors", (req, res) => {
+  res.send("Hello world!!!");
 });
 
 // Send the Requester Informations about the BackEnd
-app.get('/info', cors({ origin: '*' }), (req, res) => {
-  res.sendFile(path.join(__dirname, '../ReadMe.html'));
+app.get("/info", cors({ origin: "*" }), (req, res) => {
+  res.sendFile(path.join(__dirname, "../ReadMe.html"));
 });
 
 // Send the Requester Informations about the ErrorCodes
-app.get('/errorcodes', cors({ origin: '*' }), (req, res) => {
-  res.sendFile(path.join(__dirname, '../ErrorCodes.html'));
+app.get("/errorcodes", cors({ origin: "*" }), (req, res) => {
+  res.sendFile(path.join(__dirname, "../ErrorCodes.html"));
 });
 
 // Delete Guests every 15 min, if they are offline
@@ -53,15 +55,15 @@ setInterval(() => {
   User.deleteMany({ isGuest: true, isOnline: false });
 }, 1000 * 60 * 15);
 
-app.get('/avatars', cors({ origin: '*' }), async (req, res) => {
+app.get("/avatars", cors({ origin: "*" }), async (req, res) => {
   const query = await AvatarSchema.find();
   res.send(query);
 });
 
-app.get('/impressum/:type', cors({ origin: '*' }), async (req, res) => {
-  if (req.params.type !== 'json') {
-    res.sendFile(path.join(__dirname, '../impressum.html'));
+app.get("/impressum/:type", cors({ origin: "*" }), async (req, res) => {
+  if (req.params.type !== "json") {
+    res.sendFile(path.join(__dirname, "../impressum.html"));
   } else {
-    res.sendFile(path.join(__dirname, '../impressum.json'));
+    res.sendFile(path.join(__dirname, "../impressum.json"));
   }
 });
