f912e64925300b8f4716459ecc96a9cf23db342c
Kevin Poppe <Kevin@KevinPoppe.com>
2022-05-09 15:05:27 +0200

    added shop-system

--- routes/shopRoute.js
+++ routes/shopRoute.js
@@ -9,6 +9,7 @@ const Arcade = require("../game/Arcade");
 const UserSchema = require("../models/UserSchema");
 
 const shop = require("../shop.json");
+const { Connection } = require("../controllers/Connection");
 
 router.post("/buy", JWT.check, async (req, res) => {
   if (!req.body.userID || !req.body.itemID) {
@@ -34,6 +35,7 @@ router.post("/buy", JWT.check, async (req, res) => {
       { gold: user.gold, inventory: user.inventory }
     );
     res.status(201).send(item);
+    Connection.invalidateUserBySocketID(user.socketID);
     return;
   } else {
     res.status(402).send("You don't have enough Money!");
