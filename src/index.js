a086c891c96cb9ab0193f447cf7e30e04453f240
Kevin Poppe <Kevin@KevinPoppe.com>
2022-05-10 18:43:38 +0200

    modified cors

--- src/index.js
+++ src/index.js
@@ -27,7 +27,9 @@ app.use(
       "http://localhost:3000",
       "http://localhost:8000",
       "http://olinkirk.land",
-      "http://olinkirk.land/dont-fall/"
+      "http://olinkirk.land/dont-fall/",
+      "https://olinkirk.land",
+      "https://olinkirk.land/dont-fall/"
     ],
     credentials: true
   })
