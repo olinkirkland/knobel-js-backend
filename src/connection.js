be10f3f438ca0f0a9c91d3984fd4e728d4726fdf
Olin Kirkland <me@olinkirk.land>
2022-11-22 23:49:19 +0100

    Change mongo db access

--- src/connection.js
+++ src/connection.js
@@ -1,62 +1,62 @@
-const http = require('http');
+const http = require("http");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserController = require('../controllers/UserController');
-const gameHandler = require('../controllers/gameHandler');
-const gmaeRoute = require('../routes/game');
+const UserController = require("../controllers/UserController");
+const gameHandler = require("../controllers/gameHandler");
+const gmaeRoute = require("../routes/game");
 
 const port = process.env.PORT || 5000;
-const dbURI = `mongodb+srv://admin:${process.env.MONGO_PW}@cluster0.s1t7x.mongodb.net/test1?retryWrites=true&w=majority`;
+const dbURI = `mongodb+srv://olinkirkland:${process.env.MONGO_PW}@cluster0.p04kd.mongodb.net/?retryWrites=true&w=majority`;
 
 function connect(app) {
   const httpServer = http.createServer(app);
 
-  const io = require('socket.io')(httpServer, {
-    cors: { origin: '*', methods: ['GET', 'POST'] },
+  const io = require("socket.io")(httpServer, {
+    cors: { origin: "*", methods: ["GET", "POST"] }
   });
 
-  io.on('connection', (socket) => {
-    console.log(socket.id, 'connected');
+  io.on("connection", (socket) => {
+    console.log(socket.id, "connected");
 
     const data = {
-      userID: socket.request['_query'].userID,
+      userID: socket.request["_query"].userID,
       online: true,
-      username: socket.request['_query'].username,
+      username: socket.request["_query"].username
     };
 
     // Add new User to currentlyonlines-Collection
     UserController.changeOnlineState(data, socket.id);
 
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
       // Console.log Disconnect-Message & Update currentlyonlines-Collection
-      console.log(socket.id, 'disconnected');
+      console.log(socket.id, "disconnected");
       UserController.changeOnlineState({ online: false }, socket.id);
     });
 
-    socket.on('chat', (data, room) => {
-      if (room !== 'undefined' || room !== null || room !== '') {
+    socket.on("chat", (data, room) => {
+      if (room !== "undefined" || room !== null || room !== "") {
         socket.to(room).emit(chat);
       } else {
         // Broadcast Message to all Users, except the sending User
-        socket.broadcast.emit('chat', data);
+        socket.broadcast.emit("chat", data);
       }
     });
 
-    socket.on('game-room', (data, room) => {
+    socket.on("game-room", (data, room) => {
       socket.to(room).emit(chat);
     });
 
-    socket.on('join-game-room', (room) => {
+    socket.on("join-game-room", (room) => {
       // UserHandler.changeSocketRoom(socket.request['_query'], socket.id);
       console.log(`user joind GameRoom ${room}`);
       socket.join(room);
       // game.joinGame(socket.id, room);
     });
 
-    socket.on('join-room', (room) => {
+    socket.on("join-room", (room) => {
       // UserHandler.changeSocketRoom(socket.request['_query'], socket.id);
       console.log(`user joind Room ${room}`);
       socket.join(room);
@@ -72,7 +72,7 @@ function connect(app) {
         console.log(`Server started at http://localhost:${port}`);
       });
     })
-    .catch((err) => console.log('ERROR:', err));
+    .catch((err) => console.log("ERROR:", err));
 }
 
 module.exports = connect;
